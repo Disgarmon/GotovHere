08bfad9e1bfbfd2b5db477bd03af73f25526a72f
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-08-31 13:19:02 +0300

    fix bug with UIImage

--- GotovHere/Models/Reсipe.swift
+++ GotovHere/Models/Reсipe.swift
@@ -21,8 +21,8 @@ class Recipe{
         setup()
     }
  func setup(){
-        let p1 = Product(title: "Название1", description: "Описание1", image: "URL")
-     let p2 = Product(title: "Название2", description: "Описание2", image: "URL2")
+     let p1 = Product(title: "Название1", description: "Описание1", image: UIImage(named: "2022-08-26 14.13.12") ?? UIImage(systemName: "multiply.circle.fill")!)
+     let p2 = Product(title: "Название2", description: "Описание2", image: UIImage(named: "2022-08-26 14.13.46") ?? UIImage(systemName: "multiply.circle.fill")!)
      self.products = [p1, p2]
     }
 }
